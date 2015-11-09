f2d81566627996cfe5bdb85c8f568bf14df0f2a5
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2015-11-09 13:30:08 +0100

    fix multiple add the same product to comparasion

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,9 +4,12 @@ var ready = function() {
 
 $('.glyphicon-plus').on("click",function(){
 	var rowData = $(this).attr('id').split("#")
-	competitors.push(parseInt(rowData[0]));
-	$('#sidebar-right').find('ul').append('<li>'+rowData[1]+'</li>');
-	$('.competitors').children().append('<input type="hidden" value="'+JSON.stringify(competitors)+'" name="competitors_ids">')
+	var id = parseInt(rowData[0]);
+	if($.inArray(id, competitors) < 0){
+		competitors.push(parseInt(rowData[0]));
+		$('#sidebar-right').find('ul').append('<li>'+rowData[1]+'</li>');
+		$('.competitors').children().append('<input type="hidden" value="'+JSON.stringify(competitors)+'" name="competitors_ids">')
+	}
 });
 
 };
