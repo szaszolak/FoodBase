e5657e477c52cd827e8d416d5e749de24edecf91
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2015-11-26 03:26:29 +0100

    added logging for heroku 1

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module FoodBase
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.logger = Logger.new(STDOUT)
     config.assets.initialize_on_precompile = false
   end
 end
