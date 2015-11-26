b1c26ac2aa48f101fbb7e87a5b3f2a14481cb416
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2015-11-26 17:43:30 +0100

    product analysis view

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@
 class Product < ActiveRecord::Base
 	#acts_as_xlsx
 	belongs_to :category
+	before_save :externalize_url
 	has_many :recipes, dependent: :destroy
 	has_many :ingredients, through: :recipes
 	has_many :samples, dependent: :destroy
@@ -12,4 +13,11 @@ class Product < ActiveRecord::Base
 
 	validates :description, :name,:category_id, presence: true 
 	validates_associated :recipes
+
+	private 
+	def externalize_url
+		unless /^http:\/\//.match(self.article_url)
+			self.article_url = "http:\/\/"+self.article_url;
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126012956) do
+ActiveRecord::Schema.define(version: 20151126024338) do
 
   create_table "additives", force: :cascade do |t|
     t.string   "name"
