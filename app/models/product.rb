192309ad89954e8bf00d8e1a904ce77d98051f0d
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-02-16 00:59:27 +0100

    .

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,7 @@ class Product < ActiveRecord::Base
 	validates :description, :name,:category_id, presence: true 
 	validates_associated :recipes
 	 def pepare_charts(current_user,size="460x512")
-      @path = "app/assets/images/"+current_user.id.to_s"/";
+      @path = "app/assets/images/"+current_user.id.to_s+"/";
       FileUtils.remove_dir @path, true
 
       charts = [] 
