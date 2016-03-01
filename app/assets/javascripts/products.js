412d53cd3070b5347590137bbbb235c0c52a2cab
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-03-01 20:26:36 +0100

    test js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,12 +1,14 @@
 competitors = []
  function ready(){
   
-alert("xxxx")
+
 $('input[type="checkbox"]').on("click",function(){
 	var id = parseInt($(this).attr('id'));
 	if($.inArray(id, competitors) < 0){
+		alert("push "+id)
 		competitors.push(id);
 	}else{
+			alert("pop "+id)
 		competitors.splice(competitors.indexOf(id),1);
 	}
 	$('.competitors').children('[name="competitors_ids"]').remove()
