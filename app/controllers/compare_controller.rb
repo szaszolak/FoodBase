76243f1b76ce24fdfdb093c27869031b4ed20713
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-03-04 19:51:37 +0100

    chart generation exceptions handling

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -17,12 +17,16 @@ class CompareController < ApplicationController
 	end
 
   def get_chart
-
+    begin
      get_compared_products
      @metric = Metric.find(params.require(:metric))
      get_chart_data
      pepare_charts current_user
      send_file @path+"#{@metric.id}.png", type: "image/gif", disposition: "inline"
+     rescue  Exception => e 
+     flash[:danger] ="Nie udało się wygenerować wersji jpg wykresu. " + e.message 
+     redirect_to  action: 'index', competitors_ids: params.require(:competitors_ids)
+     end
   end
 
   private
