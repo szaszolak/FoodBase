f67dd5c195f9a6b9f966c7180e22e06b28fd909d
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-03-07 16:24:55 +0100

    small view changes

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -22,12 +22,12 @@ $('input[type="checkbox"]').on("click",function(){
 	else
 		$('.btn_competitiors').prop("disabled",true);
 });
-var dataElements = $('tr[data-link]').children(':nth-child(2),:nth-child(3),:nth-child(4),:nth-child(5),:nth-child(6)');
+var dataElements = $('tr[data-link]').children(':nth-child(2),:nth-child(3),:nth-child(4),:nth-child(5)');
 dataElements.on("click",function(){
 	 window.location = $(this.parentElement).data('link');
 });
 var hoverHandler = function(){
-	$(this.parentElement).children(':nth-child(2),:nth-child(3),:nth-child(4),:nth-child(5),:nth-child(6)').toggleClass('hover');
+	$(this.parentElement).children(':nth-child(2),:nth-child(3),:nth-child(4),:nth-child(5)').toggleClass('hover');
 };
 dataElements.hover(hoverHandler,hoverHandler);
 $('.btn_competitiors').prop("disabled",true);
@@ -37,7 +37,7 @@ $('.btn_competitiors').prop("disabled",true);
 
 function product_reset(){
 	competitors = []
-	$('tr[data-link]').children(':nth-child(2),:nth-child(3),:nth-child(4),:nth-child(5),:nth-child(6)').off();
+	$('tr[data-link]').children(':nth-child(2),:nth-child(3),:nth-child(4),:nth-child(5)').off();
 	$('input[type="checkbox"]').off();
 	
 }
