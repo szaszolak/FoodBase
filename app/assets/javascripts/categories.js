f002e4416056d2805a281edee648741f08e795d8
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-03-07 22:37:30 +0100

    ingredients and additives view improvment

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,8 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
-$('.compare.index').ready(function(){
-$(function () {
+$('.categories.index').ready(function(){
   $('[data-toggle="tooltip"]').tooltip()
-})
 })
